ed6d309c7c53ca69ba7f5fb86519257afb906592
Shakerullah <sourovmdshakerullah@gmail.com>
2026-01-15 22:05:16 +0600

    dev: define constants for dev

---
+++ src/constants.ts
@@ -0,0 +1,103 @@
+import { MatchedIssue } from "./types/issue.types";
+
+export const matched: MatchedIssue[] = [
+  {
+    repoInfo: {
+      full_name: "mdshakerullahS/OSS_Match",
+      stargazers_count: 123,
+      language: "TypeScript",
+    },
+
+    issue: {
+      id: 123,
+      title: "Fix bug",
+      html_url: "https://github.com/mdshakerullahS/OSS_Match/issues/56",
+      repository_url: "string",
+      labels: [{ name: "good first issue", color: "00ff00" }],
+      user: {
+        login: "mdshakerullahS",
+        avatar_url: "https://avatars.githubusercontent.com/u/196912697?v=4",
+      },
+      created_at: "12-01-2026",
+      updated_at: "12-01-2026",
+      body: "string",
+      comments: 123,
+      reactions: { string: 123 },
+    },
+  },
+
+  {
+    repoInfo: {
+      full_name: "mdshakerullahS/OSS_Match",
+      stargazers_count: 123,
+      language: "TypeScript",
+    },
+
+    issue: {
+      id: 123,
+      title: "Fix bug",
+      html_url: "https://github.com/mdshakerullahS/OSS_Match/issues/56",
+      repository_url: "string",
+      labels: [{ name: "good first issue", color: "00ff00" }],
+      user: {
+        login: "mdshakerullahS",
+        avatar_url: "https://avatars.githubusercontent.com/u/196912697?v=4",
+      },
+      created_at: "12-01-2026",
+      updated_at: "12-01-2026",
+      body: "string",
+      comments: 123,
+      reactions: { string: 123 },
+    },
+  },
+
+  {
+    repoInfo: {
+      full_name: "mdshakerullahS/OSS_Match",
+      stargazers_count: 123,
+      language: "TypeScript",
+    },
+
+    issue: {
+      id: 123,
+      title: "Fix bug",
+      html_url: "https://github.com/mdshakerullahS/OSS_Match/issues/56",
+      repository_url: "string",
+      labels: [{ name: "good first issue", color: "00ff00" }],
+      user: {
+        login: "mdshakerullahS",
+        avatar_url: "https://avatars.githubusercontent.com/u/196912697?v=4",
+      },
+      created_at: "12-01-2026",
+      updated_at: "12-01-2026",
+      body: "string",
+      comments: 123,
+      reactions: { string: 123 },
+    },
+  },
+
+  {
+    repoInfo: {
+      full_name: "mdshakerullahS/OSS_Match",
+      stargazers_count: 123,
+      language: "TypeScript",
+    },
+
+    issue: {
+      id: 123,
+      title: "Fix bug",
+      html_url: "https://github.com/mdshakerullahS/OSS_Match/issues/56",
+      repository_url: "string",
+      labels: [{ name: "good first issue", color: "00ff00" }],
+      user: {
+        login: "mdshakerullahS",
+        avatar_url: "https://avatars.githubusercontent.com/u/196912697?v=4",
+      },
+      created_at: "12-01-2026",
+      updated_at: "12-01-2026",
+      body: "string",
+      comments: 123,
+      reactions: { string: 123 },
+    },
+  },
+];
