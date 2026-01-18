c628a0eca13670c74f4799b4260af15581f92799
Shakerullah <sourovmdshakerullah@gmail.com>
2026-01-19 00:35:57 +0600

    update: fetch issues and render them

--- src/components/issueCard.tsx
+++ src/components/issueCard.tsx
@@ -1,14 +1,14 @@
-import { MatchedIssue } from "@/types/issue.types";
+import { GitHubIssue } from "@/types";
 import { MessageSquareText, Star } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 type IssueCardProp = {
-  issue: MatchedIssue;
+  issue: GitHubIssue;
 };
 
 export default function IssueCard({ issue }: IssueCardProp) {
-  const formattedDate = new Date(issue.issue.created_at).toLocaleDateString(
+  const formattedDate = new Date(issue.created_at).toLocaleDateString(
     undefined,
     {
       month: "short",
@@ -18,48 +18,46 @@ export default function IssueCard({ issue }: IssueCardProp) {
   );
 
   return (
-    <div className="card group p-5 hover:border-primary space-y-2">
+    <div className="card h-full p-5 hover:border-primary group space-y-2">
       {/* Header */}
       <div className="flex items-center gap-2 text-sm text-muted-foreground">
         <Image
-          src={issue.issue.user.avatar_url}
+          src={issue.user.avatar_url}
           height={48}
           width={48}
-          alt={issue.issue.user.login}
+          alt={issue.user.login}
           className="w-5 h-5 rounded-full"
         />
 
-        {/* Repo Link */}
         <div>
+          {/* Repo Link */}
           <Link
-            href={`https://github.com/${issue.repoInfo.full_name}`}
+            href={`https://github.com/${issue.repoName}`}
             target="_blank"
             rel="noopener noreferrer"
             className="font-medium hover:text-primary text-sm"
           >
-            {issue.repoInfo.full_name}
+            {issue.repoName}
           </Link>
+
+          {/* Date */}
           <p className="text-xs text-muted-foreground">{formattedDate}</p>
         </div>
       </div>
 
       {/* Title */}
       <h3 className="text-lg font-bold text-foreground leading-snug group-hover:text-primary">
-        <Link
-          href={issue.issue.html_url}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {issue.issue.title}
+        <Link href={issue.html_url} target="_blank" rel="noopener noreferrer">
+          {issue.title}
         </Link>
       </h3>
 
       {/* Labels */}
-      <div className="flex flex-wrap gap-2">
-        {issue.issue.labels?.map((label) => (
+      <div className="flex flex-wrap my-2 gap-2">
+        {issue.labels?.map((label) => (
           <span
             key={label?.name}
-            className="badge my-2"
+            className="badge"
             style={{
               backgroundColor: `#${label.color}22`,
               borderColor: `#${label.color}`,
@@ -69,26 +67,6 @@ export default function IssueCard({ issue }: IssueCardProp) {
             {label?.name}
           </span>
         ))}
-      </div>
-
-      {/* Footer Info */}
-      <div className="flex items-center justify-between pt-4 border-t text-xs text-muted-foreground">
-        <div className="flex items-center gap-3">
-          <span className="flex items-center gap-1">
-            <Star size={16} />
-            {issue.repoInfo.stargazers_count.toLocaleString()}
-          </span>
-          {issue.repoInfo.language && (
-            <span className="flex items-center gap-1">
-              <span className="w-2 h-2 rounded-full bg-ts"></span>
-              {issue.repoInfo.language}
-            </span>
-          )}
-        </div>
-        <div className="flex items-center gap-1">
-          <MessageSquareText size={16} />
-          {issue.issue.comments}
-        </div>
       </div>
     </div>
   );
--- src/components/issues.tsx
+++ src/components/issues.tsx
@@ -1,15 +1,40 @@
-import { matched } from "@/constants";
+"use client";
+
+import { GitHubIssue } from "@/types";
 import IssueCard from "./issueCard";
+import { useEffect, useState } from "react";
 
 export default function Issues() {
+  const [issues, setIssues] = useState<GitHubIssue[]>([]);
+
+  const fetchIssues = async () => {
+    try {
+      const res = await fetch("/api/issues");
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        console.log(data.message);
+        return;
+      }
+
+      setIssues(data.issues);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchIssues();
+  }, []);
+
   return (
     <div className="lg:max-h-[calc(100vh-106px)] lg:overflow-scroll lg:col-span-3 space-y-2">
-      <div className="text-sm  font-medium text-muted-foreground">{`Found ${matched?.length} matched issues`}</div>
+      <div className="text-sm  font-medium text-muted-foreground">{`Found ${issues?.length} matched issues`}</div>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-2">
-        {matched.map((m, i) => (
-          <IssueCard key={i} issue={m} />
-        ))}
+        {issues?.length &&
+          issues?.map((i, idx) => <IssueCard key={idx} issue={i} />)}
       </div>
     </div>
   );
