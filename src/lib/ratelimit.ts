7ebb85ebd40c67a8b113d7b0ce7b348f107ab9cc
Shakerullah <sourovmdshakerullah@gmail.com>
2026-01-30 17:54:36 +0600

    update: update redis configuration to work using any of upstash and redis docker image

--- src/lib/ratelimit.ts
+++ src/lib/ratelimit.ts
@@ -1,7 +1,23 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
-export const anonLimiter = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.fixedWindow(20, "1 h"),
-});
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import { redis } from "./redis";
+
+let rateLimiter: Ratelimit | RateLimiterRedis;
+
+if (process.env.REDIS_PROVIDER === "upstash") {
+  rateLimiter = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.fixedWindow(20, "1 h"),
+  });
+} else {
+  rateLimiter = new RateLimiterRedis({
+    storeClient: redis,
+    keyPrefix: "middleware",
+    points: 10,
+    duration: 60,
+  });
+}
+
+export { rateLimiter };
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,29 @@
-import { Redis } from "@upstash/redis";
+import { Redis as UpstashRedis } from "@upstash/redis";
+import Redis from "ioredis";
 
-export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+const isUpstash = process.env.REDIS_PROVIDER === "upstash";
+
+const upstash = isUpstash
+  ? new UpstashRedis({
+      url: process.env.UPSTASH_REDIS_REST_URL!,
+      token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+    })
+  : null;
+
+const ioredis = !isUpstash ? new Redis({ host: "redis", port: 6379 }) : null;
+
+export const redis = {
+  async get<T>(key: string): Promise<T | null> {
+    if (upstash) return await upstash.get<T>(key);
+    const data = await ioredis!.get(key);
+    return data ? JSON.parse(data) : null;
+  },
+
+  async set(key: string, value: any, exSeconds: number = 300) {
+    const stringValue = JSON.stringify(value);
+    if (upstash) {
+      return await upstash.set(key, stringValue, { ex: exSeconds });
+    }
+    return await ioredis!.set(key, stringValue, "EX", exSeconds);
+  },
+};
